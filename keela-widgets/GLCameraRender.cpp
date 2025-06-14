f97c57b03dc3873ae237ce1faf35c775b0114d16
Brandon Piña <brandon.pina@invista.com>
2025-06-14 10:45:06 -0400

    Following the hello-triangle tutorial from learnopengl.com

--- keela-widgets/GLCameraRender.cpp
+++ keela-widgets/GLCameraRender.cpp
@@ -4,9 +4,8 @@
 
 #include "keela-widgets/GLCameraRender.h"
 #include <glad/glad.h>
+
 Keela::GLCameraRender::GLCameraRender() {
-
-
 }
 
 Keela::GLCameraRender::~GLCameraRender() {
@@ -17,11 +16,55 @@ void Keela::GLCameraRender::on_realize() {
     GLArea::on_realize();
     make_current();
 
-    // these segfault for some reason
-    //glGenBuffers(1,&VBO);
-    //glBindBuffer(GL_ARRAY_BUFFER,VBO);
-    //glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+    // make sure opengl symbols are loaded before we do any opengl stuff
+    if (!gladLoadGL()) {
+        throw std::runtime_error("Failed to load OpenGL symbols");
+    }
 
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,GL_STATIC_DRAW);
+
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const char *vertex_cstr = vertexShaderSource.c_str();
+    glShaderSource(vertexShader, 1, &vertex_cstr, nullptr);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader,GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::stringstream ss;
+        ss << "Vertex shader compilation failed: " << infoLog;
+        throw std::runtime_error(ss.str());
+    }
+
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const char *fragment_cstr = fragmentShaderSource.c_str();
+    glShaderSource(fragmentShader, 1, &fragment_cstr, nullptr);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader,GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::stringstream ss;
+        ss << "Fragment shader compilation failed: " << infoLog;
+        throw std::runtime_error(ss.str());
+    }
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram,GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::stringstream ss;
+        ss << "Program linking failed: " << infoLog;
+        throw std::runtime_error(ss.str());
+    }
 }
 
 bool Keela::GLCameraRender::on_render(const Glib::RefPtr<Gdk::GLContext> &context) {
--- keela-widgets/keela-widgets/GLCameraRender.h
+++ keela-widgets/keela-widgets/GLCameraRender.h
@@ -7,19 +7,43 @@
 #include <gtkmm/glarea.h>
 
 namespace Keela {
-    class GLCameraRender final : public Gtk::GLArea{
+    class GLCameraRender final : public Gtk::GLArea {
     public:
         GLCameraRender();
+
         ~GLCameraRender() override;
+
     protected:
         void on_realize() override;
 
         bool on_render(const Glib::RefPtr<Gdk::GLContext> &context) override;
+
     private:
-        float vertices[9]={  -0.5f,-0.5f,0.0f,
-                            0.5f,-0.5f,0.0f,
-                            0.0f,0.5f,0.0f};
+        float vertices[9] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+        };
+
+        // TODO: look into gio/resource to embed the shader code into our binary
+        const std::string vertexShaderSource = "#version 330 core\n"
+                "layout (location = 0) in vec3 aPos;\n"
+                "void main()\n"
+                "{\n"
+                "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                "}\0";
+
+        const std::string fragmentShaderSource = "#version 330 core\n"
+                "out vec4 FragColor;\n"
+                "void main()\n"
+                "{\n"
+                "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                "}\0";
+
         unsigned int VBO;
+        unsigned int vertexShader;
+        unsigned int fragmentShader;
+        unsigned int shaderProgram;
     };
 }
 #endif //GLCAMERARENDER_H
