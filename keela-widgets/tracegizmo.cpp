c84ba9351af3b2244e6cc63198573d1e98ad4ba0
Brandon Piña <brandon.pina@invista.com>
2025-06-28 13:01:55 -0400

    ellipse transformation fixed but invalid matrix exception needs work

--- keela-widgets/tracegizmo.cpp
+++ keela-widgets/tracegizmo.cpp
@@ -11,10 +11,12 @@ Keela::TraceGizmo::TraceGizmo() {
     add_events(Gdk::POINTER_MOTION_MASK | Gdk::BUTTON_RELEASE_MASK | Gdk::BUTTON_PRESS_MASK);
     //center = Gdk::Point(250, 250);
     bounds = Gdk::Rectangle(250, 250, 100, 200);
-    auto center_x = bounds.get_x() / 2;
-    auto center_y = bounds.get_y() / 2;
-    auto off_x = bounds.get_width() / 2;
-    auto off_y = bounds.get_height() / 2;
+
+    auto off_x = HALF(bounds.get_width());
+    auto off_y = HALF(bounds.get_height());
+
+    auto center_x = bounds.get_x() + off_x;
+    auto center_y = bounds.get_y() + off_y;
     ctrl_top_left = std::make_unique<GizmoControl>(center_x - off_x, center_y - off_y);
     ctrl_top_right = std::make_unique<GizmoControl>(center_x + off_x, center_y - off_y);
     ctrl_bottom_left = std::make_unique<GizmoControl>(center_x - off_x, center_y + off_y);
@@ -62,10 +64,10 @@ bool Keela::TraceGizmo::on_motion_notify_event(GdkEventMotion *motion_event) {
 
     if (ctrl_top_left != nullptr & ctrl_top_right != nullptr & ctrl_bottom_left != nullptr & ctrl_bottom_right !=
         nullptr) {
-        ctrl_top_left->set_hovered(ctrl_top_left->intersects(pt) & (mode == top_left || mode == none));
-        ctrl_top_right->set_hovered(ctrl_top_right->intersects(pt) & (mode == top_right || mode == none));
-        ctrl_bottom_left->set_hovered(ctrl_bottom_left->intersects(pt) & (mode == bottom_left || mode == none));
-        ctrl_bottom_right->set_hovered(ctrl_bottom_right->intersects(pt) & (mode == bottom_right || mode == none));
+        ctrl_top_left->set_hovered((ctrl_top_left->intersects(pt) & mode == none) || mode == top_left);
+        ctrl_top_right->set_hovered((ctrl_top_right->intersects(pt) & mode == none) || mode == top_right);
+        ctrl_bottom_left->set_hovered((ctrl_bottom_left->intersects(pt) & mode == none) || mode == bottom_left);
+        ctrl_bottom_right->set_hovered((ctrl_bottom_right->intersects(pt) & mode == none) || mode == bottom_right);
 
         GizmoControl *control = nullptr;
         switch (mode) {
@@ -87,8 +89,9 @@ bool Keela::TraceGizmo::on_motion_notify_event(GdkEventMotion *motion_event) {
         assert(control != nullptr);
         control->set_center(pt);
     }
-    bounds.set_x(ctrl_top_left->get_x());
-    bounds.set_y(ctrl_top_left->get_y());
+
+    bounds.set_x(ctrl_top_left->get_center().get_x());
+    bounds.set_y(ctrl_top_left->get_center().get_y());
 
     auto width = ctrl_top_right->get_x() - ctrl_top_left->get_x();
     auto height = ctrl_bottom_left->get_y() - ctrl_top_left->get_y();
@@ -103,15 +106,15 @@ bool Keela::TraceGizmo::on_draw(const Cairo::RefPtr<Cairo::Context> &cr) {
     cr->set_line_width(2);
     if (ctrl_top_left != nullptr & ctrl_top_right != nullptr & ctrl_bottom_left != nullptr & ctrl_bottom_right !=
         nullptr) {
-        cr->set_source_rgb(0, 1, 0);
+        //cr->set_source_rgb(0, 1, 0);
         ctrl_top_left->draw(cr);
-        cr->set_source_rgb(1, 0, 0);
+        //cr->set_source_rgb(1, 0, 0);
         ctrl_top_right->draw(cr);
         ctrl_bottom_left->draw(cr);
         ctrl_bottom_right->draw(cr);
     }
     cr->save();
-    cr->translate(HALF(bounds.get_x()), HALF(bounds.get_y()));
+    cr->translate(bounds.get_x() + HALF(bounds.get_width()), bounds.get_y() + HALF(bounds.get_height()));
     cr->scale(HALF(bounds.get_width()), HALF(bounds.get_height()));
     cr->translate(-HALF(bounds.get_x()), -HALF(bounds.get_y()));
     cr->arc(HALF(bounds.get_x()), HALF(bounds.get_y()), 1, 0, 2 * M_PI);
@@ -119,8 +122,8 @@ bool Keela::TraceGizmo::on_draw(const Cairo::RefPtr<Cairo::Context> &cr) {
     cr->stroke();
 
     cr->set_source_rgb(0, 0, 1);
-    cr->rectangle(bounds.get_x(), bounds.get_y(), bounds.get_width(), bounds.get_height());
-    cr->stroke();
+    //cr->rectangle(bounds.get_x(), bounds.get_y(), bounds.get_width(), bounds.get_height());
+    //cr->stroke();
     cr->restore();
     return DrawingArea::on_draw(cr);
 }
