2b928675b2efedf7f02f105893512789fe79fc97
Brandon Piña <brandon.pina@invista.com>
2025-07-06 15:03:48 -0400

    clamp uniforms to a certain range

--- keela-widgets/shaders/trace-vertex.glsl
+++ keela-widgets/shaders/trace-vertex.glsl
@@ -10,9 +10,13 @@ uniform uint numSamples;
 sampleMax and sampleMin are minimum and maximum values for the signals in normalized coordinates
 */
 uniform float sampleMax = 1.0;
-uniform float sampleMin = -1.0;
+uniform float sampleMin = 0;
 
 void main() {
-    float scale = sampleMax - sampleMin;
-    gl_Position = vec4((float(point.x) * 2 / numSamples) - 1, scale * point.y + sampleMin, 0.0, 1.0);
+    // confine min and max to be within the range 0-1 inclusive
+    float max = clamp(sampleMax, 0, 1);
+    float min = clamp(sampleMin, 0, max);
+
+    float scale = max - min;
+    gl_Position = vec4((float(point.x) * 2 / numSamples) - 1, scale * point.y + min, 0.0, 1.0);
 }
