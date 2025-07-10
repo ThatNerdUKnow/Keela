59dfcc4731daa679aefee8832feabaa30904300f
Brandon Piña <brandon.pina@invista.com>
2025-07-09 20:02:48 -0400

    shader should now be agnostic of numeric limits of the source signal

--- keela-widgets/shaders/trace-vertex.glsl
+++ keela-widgets/shaders/trace-vertex.glsl
@@ -10,14 +10,12 @@ uniform uint numSamples;
 sampleMax and sampleMin are minimum and maximum values for the signals in normalized coordinates
 */
 uniform float sampleMax = 1.0;
-uniform float sampleMin = 0;
+uniform float sampleMin = -1.0;
 
 void main() {
-    // confine min and max to be within the range 0-1 inclusive
     float x = gl_VertexID;
-    float max = clamp(sampleMax, 0, 1);
-    float min = clamp(sampleMin, 0, max);
 
-    float scale = max - min;
-    gl_Position = vec4((x * 2 / numSamples) - 1, scale * y + min, 0.0, 1.0);
+    float scale = 1 / sampleMax;
+    float min = -1.0 - sampleMin;
+    gl_Position = vec4((x * 2 / numSamples) - 1, scale * y - min, 0.0, 1.0);
 }
