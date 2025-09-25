42ca1024c7c5de6b3db2c749502489bd999bba5f
Brandon Piña <32885853+ThatNerdUKnow@users.noreply.github.com>
2025-09-24 20:41:00 -0400

    update tests

--- keela-test/widgets.cpp
+++ keela-test/widgets.cpp
@@ -5,7 +5,22 @@
 
 #include <keela-widgets/GLCameraRender.h>
 
+#include "keela-pipeline/consts.h"
+#include "keela-widgets/cameramanager.h"
+
 TEST(KeelaWidgets, ConstructGLCameraRender) {
     auto bin = std::make_shared<Keela::PresentationBin>();
     auto c = Keela::GLCameraRender(bin);
+}
+
+TEST(KeelaWidgets, ConstructCameraManagerGRAY8) {
+    auto cm = Keela::CameraManager(0, GRAY8, false);
+}
+
+TEST(KeelaWidgets, ConstructCameraManagerGRAY16LE) {
+    auto cm = Keela::CameraManager(0, GRAY16_LE, false);
+}
+
+TEST(KeelaWidgets, ConstructCameraManagerGRAY16BE) {
+    auto cm = Keela::CameraManager(0, GRAY16_BE, false);
 }
